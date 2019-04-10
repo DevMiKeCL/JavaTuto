0d39fde327f4e25909a700caa35e3def57400c1d
Miguel <miguelperaltad@gmail.com>
2019-04-10 17:27:13 -0300

    Hasta Sección 18

--- src/seccion018/Clase068.java
+++ src/seccion018/Clase068.java
@@ -2,16 +2,16 @@ package seccion018;
 
 public class Clase068 {
     public static void main(String [] args){
-        Persona p1 = new Persona();
+        Persona p1 = new Persona("Mike", "DJ", true);
         //Marca error
         //No se puede acceder directamente un atributo privado desde otra clase
         //p1.nombre = "Pedro";
         //Si es posible acceder a un metodo o atributo publico desde otra clase
-        //p1.obtenerNombre();
+        System.out.println(p1);
 
         //modificamos el atributo nombre
-        p1.setNombre("MiKe");
-        p1.setApellidoPaterno("DJ");
+        p1.setNombre("Katherine");
+        p1.setApellidoPaterno("KaM");
         //accedemos al atributo nombre
         System.out.println("Nombre: " + p1.getNombre() + p1.getApellidoPaterno());
     }
--- src/seccion018/Persona.java
+++ src/seccion018/Persona.java
@@ -4,7 +4,13 @@ public class Persona {
     //Atributos privados
     private String nombre;
     private String apellidoPaterno;
+    private boolean borrado;
 
+    public Persona(String nombre, String apellidoPaterno, boolean borrado){
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.borrado = borrado;
+    }
     //metodo público para acceder al atributo
     public String getApellidoPaterno() {
         return apellidoPaterno;
@@ -23,5 +29,17 @@ public class Persona {
     //metodo público para modificar el atributo
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public boolean isBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(boolean borrado) {
+        this.borrado = borrado;
+    }
+
+    public String toString(){
+        return "Nombre: " + nombre + " Apellido: " + apellidoPaterno + " borrado: " + borrado;
     }
 }
