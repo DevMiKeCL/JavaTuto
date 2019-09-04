9743cad5dadf75b3b2374e83df5cb9408e29d7f9
Miguel <miguelperaltad@gmail.com>
2019-09-04 17:40:56 -0400

    Iniciando el curso Oracle Certified Associate

---
+++ src/OCA/cap01/packagea/ClassA.java
@@ -0,0 +1,4 @@
+package OCA.cap01.packagea;
+
+public class ClassA {
+}
---
+++ src/OCA/cap01/packageb/ClassB.java
@@ -0,0 +1,10 @@
+package OCA.cap01.packageb;
+
+import OCA.cap01.packagea.ClassA;
+
+public class ClassB {
+    public static void main(String[] args) {
+        ClassA a;
+        System.out.println("Got it!");
+    }
+}
